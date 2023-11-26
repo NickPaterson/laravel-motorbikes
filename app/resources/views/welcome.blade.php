a68d95683854cea8d19ea3431d71b3b763d8bda5
NickPaterson <npgb18@gmail.com>
2023-11-26 15:21:52 +0000

    .

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Motorbikes For Sale</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
